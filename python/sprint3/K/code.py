fc7fe1d45f9a4bcf1d849e620ce6302bc6dbd0a4
Kirill Ushakov <baby-escalade@yandex.ru>
2022-12-08 20:08:33 +0300

    sprint3_nonfinal

--- python/sprint3/K/code.py
+++ python/sprint3/K/code.py
@@ -1,56 +1,59 @@
 def merge(arr, lf, mid, rg):
-	# Your code
-	# “ヽ(´▽｀)ノ”
-	pass
+    left = arr[lf:mid]
+    right = arr[mid:rg]
+    result = []
+    l, r = 0, 0
+    while l < len(left) and r < len(right):
+        if left[l] <= right[r]:
+            result.append(left[l])
+            l += 1
+        else:
+            result.append(right[r])
+            r += 1
+    
+    while l < len(left):
+        result.append(left[l])
+        l += 1
+    while r < len(right):
+        result.append(right[r])
+        r += 1
+    n = 0
+    for i in range(lf, rg):
+        arr[i] = result[n]
+        n+=1
+        
+    return arr
 
 
 def merge_sort(arr, lf, rg):
-	if len(arr) == 1:  # базовый случай рекурсии
-		return arr
+    if rg - lf <= 1:
+        return
+    mid = (lf + rg) // 2
+    merge_sort(arr, lf, mid)
+    merge_sort(arr, mid, rg)
 
-	# запускаем сортировку рекурсивно на левой половине
-	left = merge_sort(array[0 : len(array)/2])
+    merge(arr, lf, mid, rg)
 
-	# запускаем сортировку рекурсивно на правой половине
-	right = merge_sort(array[len(array)/2 : len(array)])
 
-	# заводим массив для результата сортировки
-	result = [] * len(array)
+func = str(input())
+n = int(input())
+massive = list(map(int, input().strip().split()))
+if func == 'merge':
+    massive = merge(massive, 0, n // 2, n)
+elif func == 'sort':
+    merge_sort(massive, 0, n)
+print(*massive, sep=' ')
 
-	# сливаем результаты
-	l, r, k = 0, 0, 0
-	while l < len(left) and r < len(right): 
-	# выбираем, из какого массива забрать минимальный элемент
-		if left[l] <= right[r]: 
-			result[k] = left[l]
-			l += 1
-		else:
-			result[k] = right[r]
-			r += 1
-		k += 1
 
-  # Если один массив закончился раньше, чем второй, то
-  # переносим оставшиеся элементы второго массива в результирующий
-	while l < len(left):
-		result[k] = left[l] # перенеси оставшиеся элементы left в result
-		l += 1
-		k += 1  
-	while r < len(right): 
-		result[k] = right[r] # перенеси оставшиеся элементы right в result
-		r += 1
-		k += 1
-  
-	return result 
+# def test():
+#     a = [1, 4, 9, 2, 10, 11]
+#     b = merge(a, 0, 3, 6)
+#     expected = [1, 2, 4, 9, 10, 11]
+#     assert b == expected
+#     c = [17, 7]
+#     merge_sort(c, 0 , 2)
+#     expected = [7, 17]
+#     assert c == expected
 
-def test():
-	a = [1, 4, 9, 2, 10, 11]
-	b = merge(a, 0, 3, 6)
-	expected = [1, 2, 4, 9, 10, 11]
-	assert b == expected
-	c = [1, 4, 2, 10, 1, 2]
-	merge_sort(c, 0 , 6)
-	expected = [1, 1, 2, 2, 4, 10]
-	assert c == expected
-
-if __name__ == '__main__':
-    test()
+# if __name__ == '__main__':
+#     test()
